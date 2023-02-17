607bb8d2503a7bded2a945793339f94596b6bd59
Greg Tambornino <greg.tambornino@concordusa.com>
2023-02-17 16:50:13 -0600

    initialize a basic patch

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ const bodyParser = require( 'body-parser' );
 const cors = require('cors');
 const PORT = 5000;
 
-let taskList = [{
+const incompleteTaskList = [{
     "id": 1,
     "name": "Write a to-do list in AngularJS",
     "details": "That's what this is...",
     "type": "",
     "date": "Thu Feb 16 2023 15:07:16 GMT-0600 (Central Standard Time)"
 }];
+const completedTaskList = [];
+
 // Create our app
 const app = express();
 
@@ -24,14 +26,22 @@ app.use(express.static('../app/index.html'));
 app.post('/newtask', (req, res) => {
     console.log('received new task: ', req.body);
     const newTaskToAdd = req.body;
-    taskList.push(newTaskToAdd);
+    incompleteTaskList.push(newTaskToAdd);
     res.send({ message: 'Success!' });
 });
 
 // req is request, res is the response
 app.get('/alltasks', (req, res) => {
     console.log('in GET /items');
-    res.send(taskList);
+    res.send(incompleteTaskList);
+});
+
+app.patch('/:id', (req, res) => {
+    const taskId = req.params.id;
+    let index = incompleteTaskList.indexOf(x => x.id === taskId)
+    let completedTask = incompleteTaskList.splice(index, 1);
+    completedTaskList.push(completedTask);
+    res.send({message: `removed ${taskId}`});
 });
 
 // Start the server
